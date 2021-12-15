63b8434548962e805a88b2efb5b0196384d322ed
JulianCampolongo <juliancampolongo7@gmail.com>
2021-12-15 13:35:08 -0300

    se cambia foto de loki por luca

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 let movies = [
     {
-        name: 'loki',
-        des: 'Hola, soy Julian Campolongo, y estoy creando una clonacion de la pagina web de Disney+, estoy estudiando desarrollo web y estoy en busqueda de mi primer empleo como desarrollador, si te gusta lo que estoy haciendo con una reaccion a esta publicacion me ayudas muchisimo !',
-        image: 'img/slider 1.PNG'
+        name: 'Hola gente linda de Linkedin',
+        des: ' Soy Julian Campolongo, y estoy creando una clonacion de la pagina web de Disney+, estoy estudiando desarrollo web y estoy en busqueda de mi primer empleo como desarrollador, si te gusta lo que estoy haciendo con una reaccion a esta publicacion me ayudas muchisimo !',
+        image: 'img/slider 5.PNG'
     },
     {
         name: 'loki',
