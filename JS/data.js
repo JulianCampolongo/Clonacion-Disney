55b8a86cedb2248c9b30a898ce40645a7acbb06f
JulianCampolongo <juliancampolongo7@gmail.com>
2022-02-01 01:34:44 -0300

    modificar mapeo

---
+++ JS/data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'La Niñera',
+        des: 'Hola, soy Julian Campolongo y estoy realizando una web de series y pelis de los 90! Ojala les guste! Me ayudan muchisimo con una reaccion o un comment! Muchas gracias :):):)',
+        image: 'img/fine2.jpeg'
+    },
+    {
+        name: ' Padre De Familia',
+        des: 'Hola, soy Julian Campolongo y estoy realizando una web de series y pelis de los 90! Ojala les guste! Me ayudan muchisimo con una reaccion o un comment! Muchas gracias :):):)',
+        image: 'img/drepa.jpg'
+    },
+    {
+        name: 'Dragon Ball Z',
+        des: 'Hola, soy Julian Campolongo y estoy realizando una web de series y pelis de los 90! Ojala les guste! Me ayudan muchisimo con una reaccion o un comment! Muchas gracias :):):)',
+        image: 'img/dbz1.jpg'
+    },
+    {
+        name: 'Slam Dunk',
+        des: 'Hola, soy Julian Campolongo y estoy realizando una web de series y pelis de los 90! Ojala les guste! Me ayudan muchisimo con una reaccion o un comment! Muchas gracias :):):)',
+        image: 'img/SLAM.jpg'
+    },
+    {
+        name: 'One Piece',
+        des: 'Hola, soy Julian Campolongo y estoy realizando una web de series y pelis de los 90! Ojala les guste! Me ayudan muchisimo con una reaccion o un comment! Muchas gracias :):):)',
+        image: 'img/one.jpg'
+    }
+]
